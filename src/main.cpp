534955afd3d29948d39259d63c18f3d175351ae1
Dronquavious <tambweamani@gmail.com>
2025-12-28 17:29:26 -0600

    added seed gen to randomize the seed based on current time

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "raylib.h"
 #include "game.h"
+#include <ctime>
 
 int main()
 {
     InitWindow(1280, 720, "VOXEL SANDBOX");
     SetTargetFPS(500);
 
+    SetRandomSeed((unsigned int)time(0));
     Game game;
 
     game.Init();
