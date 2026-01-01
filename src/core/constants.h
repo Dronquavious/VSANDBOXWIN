54935bda067b1da70e3e0c1f3ab1d92a1db2ae63
Dronquavious <tambweamani@gmail.com>
2026-01-01 15:35:36 -0600

    added sea level and increased chunk size

--- src/core/constants.h
+++ src/core/constants.h
@@ -2,8 +2,9 @@
 #define CONSTANTS_H
 
 // world settings
-#define CHUNK_SIZE 32
-#define RENDER_DISTANCE 12
+#define CHUNK_SIZE 64
+#define RENDER_DISTANCE 8
+#define SEA_LEVEL 28
 
 // texture settings
 #define BLOCK_TEX_SIZE 16
